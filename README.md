a82c2989f3c58805360c1868aa64982a24728944
William Firmino <willian.firmino1@gmail.com>
2022-01-30 20:45:27 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# angular-the-complete-guide is the project from the course found at udemy.com
---
+++ my-first-app/src/app/server/server/server.component.ts
@@ -0,0 +1,28 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-server',
+  templateUrl: './server.component.html',
+  styleUrls: ['./server.component.css']
+})
+export class ServerComponent implements OnInit {
+
+  serverId = 10;
+  serverStatus = '';
+
+  constructor() {
+    this.serverStatus = Math.random() > 0.5 ? 'online' : 'offline'
+  }
+
+  ngOnInit(): void {
+  }
+
+  getServerStatus() {
+    return this.serverStatus;
+  }
+
+  getColor() {
+    return this.serverStatus === 'online' ? 'green' : 'red';
+  }
+
+}
